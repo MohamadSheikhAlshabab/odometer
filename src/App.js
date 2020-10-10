bd3c110d05692d630c5e81b38cebe5fac128b426
MohamadSheikhAlshabab <alshabab.moh@gmail.com>
2020-10-10 17:35:38 +0300

    odometer done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,104 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function Header(props) {
+  console.log(props);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header className="Header ">
+      <p> React is Awesome {props.text}</p>
+    </header>
   );
 }
 
-export default App;
+
+function Footer(props) {
+  console.log(props);
+  return (
+    <footer className="Footer">
+      <p>Welcome from Footer {props.trademark}</p>
+    </footer>
+  );
+}
+
+class Odometer extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+
+      counter: '0000'
+    }
+
+  }
+
+  add(e) {
+    console.log(document.querySelector('.odometer-value').value);
+    let odometerValue = document.querySelector('.odometer-value')
+
+    function zeroPad(num, places) {
+      var zero = places - num.toString().length + 1;
+      return Array(+(zero > 0 && zero)).join("0") + num;
+    }
+    let currentOdometerValue = Number(odometerValue.value);
+    console.log(currentOdometerValue)
+
+    let test = currentOdometerValue + e;
+    console.log('test')
+    if (test > 9999) {
+      odometerValue.value = 0
+      odometerValue.value += test % 10000
+      odometerValue.value = zeroPad(odometerValue.value, 4)
+    } else {
+      odometerValue.value = test
+      odometerValue.value = zeroPad(odometerValue.value, 4)
+    }
+
+    console.log(odometerValue.value)
+    this.setState({
+      counter: odometerValue.value
+    })
+
+  }
+  render() {
+    return (
+      <>
+        <h1>Odometer</h1>
+        <div class="container">
+          <div class="odometer-module">
+            <div class="odometer-buttons odometer-buttons-top">
+              <button onClick={() => this.add(1)} class="odometer-button js-odometer-button"> +1</button>
+              <button onClick={() => this.add(10)} class="odometer-button js-odometer-button"> +10</button>
+              <button onClick={() => this.add(100)} class="odometer-button js-odometer-button"> +100</button>
+              <button onClick={() => this.add(1000)} class="odometer-button js-odometer-button"> +1000</button>
+            </div>
+            <input class="odometer-value" type="number" value={this.state.counter} max="9999"></input>
+          </div>
+        </div>
+        <h3>odometer-value: {this.state.counter}</h3>
+
+      </>
+    );
+  }
+
+}
+
+function Main(props) {
+  return (
+
+    <main>
+      <div className="Main">
+        <h1>Hello</h1>
+      </div>
+      <Odometer />
+    </main>
+  );
+}
+export default function App() {
+
+  return (
+    <>
+      <Header text="hello from Header" />
+      <Main />
+      <Footer trademark="trademark" />
+    </>
+  );
+}
